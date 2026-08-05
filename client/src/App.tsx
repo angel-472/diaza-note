64cde522ed58f4acad3915ef059ebe65cc8380c3
angel <108492882+angel-472@users.noreply.github.com>
2026-08-05 11:04:25 -0400

    Create React scaffolding for the frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="p-8">Luno Note</p>
+    </>
+  )
+}
+
+export default App
